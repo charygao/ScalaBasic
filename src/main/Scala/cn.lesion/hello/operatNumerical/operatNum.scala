49c2693242872477f6ebf3eed3307f4577f813f7
gmle <e.le.lee.leee@gmail.com>
2016-07-24 22:42:44 +0800

    增加基础方法与操作。

--- src/main/Scala/cn.lesion/hello/operatNumerical/operatNum.scala
+++ src/main/Scala/cn.lesion/hello/operatNumerical/operatNum.scala
@@ -47,12 +47,59 @@ object operatNum extends App {
 
     val charStr = "100"
 
-    println("Int:" + charStr.toInt)
+//    println("Int:" + charStr.toInt)
     println("Long:" + charStr.toLong)
     println("Short" + charStr.toShort)
     println("Float:" + charStr.toFloat)
     println("Double:" + charStr.toDouble)
     println("Byte:" + charStr.toByte)
+
+
+    """
+      |scala 中的 toInt 不支持传入进制，为了解决这个问题，可以使用java.lang.Integer 类中的 parseInt方法
+    """.stripMargin
+
+    println(Integer.parseInt("2",10))
+    println(Integer.parseInt("8",10))
+    println(Integer.parseInt("10",10))
+    println(Integer.parseInt("16",10))
+/*
+
+    //如果喜欢隐式转换，可以创建隐式转换类来解决这个问题
+    implicit class String2Int(str: String){
+      def toInt(radix :Int) = Integer.parseInt(str, radix)
+    }
+
+    println(String2Int(charStr).toString)
+
+*/
+
+    //
+    """
+      |java代码调用这个方法时，这个注解式异常是必须的。
+      |但是在Scala中，通常使用Option/Some/None来解决问题。
+    """.stripMargin
+//    @throws(classOf[NumberFormatException])
+//    def toInt(s: String) = s.toInt
+
+    def toInt(strAbc: String): Option[Int] ={
+      try{
+        Some(strAbc.toInt)
+      }catch {
+        case e: NumberFormatException => None
+      }
+    }
+
+    //根据需要通过多种方式调用toInt方法，其中一种就是通过getOrElse：
+    println(toInt("1").getOrElse(0)) //1
+    println(toInt("2").getOrElse(0)) //0
+
+    println(toInt(charStr).getOrElse())
+
+
+
+
+
   }
 
 }
