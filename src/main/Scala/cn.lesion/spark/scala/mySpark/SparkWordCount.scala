05b1990fe791c79b14219cf24ae324d4e9967bc6
gmle <e.le.lee.leee@gmail.com>
2016-08-31 23:39:07 +0800

    细节修改

--- src/main/Scala/cn.lesion/spark/scala/mySpark/SparkWordCount.scala
+++ src/main/Scala/cn.lesion/spark/scala/mySpark/SparkWordCount.scala
@@ -13,19 +13,24 @@ object SparkWordCount {
     val conf = new SparkConf().setAppName("Spark WordCount").setMaster("local[*]")
     val spark = new SparkContext(conf)
 
-    //读取HDFS文件并检测单词 “AUDJPY”
+    //获取当前项目根目录
+    val filePath=System.getProperty("user.dir")
+
+    print(filePath)
+
     //将数据缓存到内存（数据量大的时候，内存不足会造成内存溢出）
     val file = spark.textFile("hdfs://love:9000/Data/test/webrank.txt").cache()
     //执行过滤。
 //      .filter(_.contains("AUDJPY"))
 
+    //存储到本地之前先删除目录下的结果
     //将结果存储到本地
-    file.saveAsTextFile("/home/lele/文档/Codes/Intellij_Idea/ScalaBasic/src/main/resources/Test/WordCount")
+    file.saveAsTextFile(filePath + "/Test/WordCount")
 
     file.flatMap(line => line.split(" ")) //分割
       .map(data => (data,1))              //统计value为1的单词（映射为key-value对）
       .reduceByKey(_+_)                   //对同一个a第n次的数据进行聚集，统计单词个数
-      .foreach(println)                   //输出。
+//      .foreach(println)                   //输出。
 
     //统计所有单词个数
     println("Result =  " + file.count)
