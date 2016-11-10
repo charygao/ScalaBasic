9af4febb5eb46cfb063582ca0cef5afc6a7360ce
gmle <e.le.lee.leee@gmail.com>
2016-11-10 18:15:06 +0800

    change

--- src/main/Scala/cn.lesion/hello/start/MuModule.scala
+++ src/main/Scala/cn.lesion/hello/start/MuModule.scala
@@ -11,7 +11,7 @@ package cn.lesion.hello.start
 //一行注释
 /*另一行注释*/
 /**文档说明*/
-object MuModule { // 生命一个单例对象，即同时声明一个类和它的唯一实例。
+object MuModule { // 声明一个单例对象，即同时声明一个类和它的唯一实例。
   def abs(n: Int): Int =  //abs方法接收一个integer并返回一个integer。
     if (n < 0) -n     //如果n小于0 返回 -n
     else n
@@ -35,7 +35,5 @@ object MuModule { // 生命一个单例对象，即同时声明一个类和它�
   *   Object关键字创建一个新的单例类型，就像一个class只有一个被命名的实例
   *
   * MyModule对象有三个方法，它们使用def关键字生命：abs formatAbs和main
-  *
-  *
   *
   */
