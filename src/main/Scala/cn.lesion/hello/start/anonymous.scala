7f2d9af7bee7f81870a81e374622fda1aae15f37
王乐 <e.le.lee.leee@gmail.com>
2016-12-08 19:44:17 +0800

    内部匿名累

---
+++ src/main/Scala/cn.lesion/hello/start/anonymous.scala
@@ -0,0 +1,31 @@
+package cn.lesion.hello.start
+
+/**
+  * Created for Intellij IDEA.
+  * Author:e.le.lee.
+  * Date: 2016/12/8.
+  */
+
+class Person(val name:String){
+
+}
+class anonymous(codeName:String,age:Int) extends Person(codeName){
+
+  override
+    val name:String = "xxx"
+
+  def meet(p:Person{def greeting:String}): Unit ={
+    println("p.greeting")
+  }
+
+}
+
+object test{
+  def main(args: Array[String]): Unit = {
+//    println(new anonymous.meet("tom",12))
+    val x = new anonymous("tom",12)
+    x.meet(new Person("tom"){
+      def greeting:String="hello"
+    })
+  }
+}
